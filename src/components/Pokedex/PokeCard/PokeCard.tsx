fcec71e1c228c02b76775f841ee4811b49aef32e
Joshua Nance <joshua.h.nance@gmail.com>
2022-11-13 17:51:41 -0800

    Call for pokemon details from within each card

--- src/components/Pokedex/PokeCard/PokeCard.tsx
+++ src/components/Pokedex/PokeCard/PokeCard.tsx
@@ -1,13 +1,47 @@
-import React from "react";
-import "./PokeCard.css";
+// @ts-nocheck
+import React, { useEffect, useState } from 'react';
+import './PokeCard.css';
 
-const PokeCard = (props: any) => {
-    return (
-        <div className="pokecard">
-            <img src={props.url} alt="Default for Pokemon"/>
-            <p>{props.name}</p>
-        </div>
-    )
+function useSinglePokemonDetails(pokemon: any) {
+  const [details, setDetails] = useState(null);
+
+  useEffect(
+    function () {
+      async function fetchDetails() {
+        try {
+          const pokemonDetailsResponse = await fetch(pokemon.url);
+
+          if (!pokemonDetailsResponse.ok) {
+            throw Error(`${response.status}: ${response.statusText}`);
+          }
+
+          setDetails(await pokemonDetailsResponse.json());
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      fetchDetails();
+    },
+    [pokemon.url]
+  );
+
+  return details;
 }
 
-export default PokeCard;
+export default function PokeCard(props: any) {
+  const pokemonDetails: any = useSinglePokemonDetails(props.pokemon);
+
+  // You probably should do something else here for CLS
+  if (!pokemonDetails) return null;
+
+  return (
+    <div key={pokemonDetails?.id} className="pokecard">
+      <img
+        src={pokemonDetails?.sprites?.front_default}
+        alt="Default for Pokemon"
+      />
+      <p>{pokemonDetails?.name}</p>
+    </div>
+  );
+}
--- src/components/Pokedex/PokeList/PokeList.tsx
+++ src/components/Pokedex/PokeList/PokeList.tsx
@@ -1,21 +1,13 @@
-import React from "react";
-import PokeCard from "../PokeCard/PokeCard";
-import "./PokeList.css";
+import React from 'react';
+import PokeCard from '../PokeCard/PokeCard';
+import './PokeList.css';
 
-
-export default function PokeList(props: any){
-    return (
-        <div className="pokelist">
-            {
-                props.list.map((pokeStuff: any) => {
-                    console.log(pokeStuff.id);
-                    return <PokeCard 
-                        id={pokeStuff.id}
-                        name={pokeStuff.name} 
-                        url={pokeStuff.sprites.front_default}
-                    />
-                })
-            }
-        </div>
-    )
+export default function PokeList(props: any) {
+  return (
+    <div className="pokelist">
+      {props.list?.map((pokemon: any) => {
+        return <PokeCard pokemon={pokemon} />;
+      })}
+    </div>
+  );
 }
